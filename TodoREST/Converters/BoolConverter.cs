d447ecf4b7e1db2ee010005d6e17026d6b345dd9
Uli Petersen <dev@upworx.net>
2020-12-20 16:08:30 +0100

    added to git (forgotten in first instance

---
+++ TodoREST/Converters/BoolConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CryptoGrabber
+{
+
+    // [ValueConversion(typeof(DateTime), typeof(bool))]
+    public class BoolConverter : IValueConverter
+    {
+
+        // checks whether a given Double (value) is greater than the parameter or not and, if so, returns "true" else "false"
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return false;
+
+            Boolean _value = (Boolean)value;
+
+            if (_value == true)
+                return true;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
---
+++ TodoREST/Converters/DateConverter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CryptoGrabber
+{
+
+    // [ValueConversion(typeof(DateTime), typeof(bool))]
+    public class DateConverter : IValueConverter
+    {
+        // checks whether a given Date (value) is more than or equal X days (parameter) away from today's date
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return false;
+
+            string valueString = value.ToString();
+            CultureInfo MyCultureInfo = new CultureInfo("de-DE");
+            int i = 0;
+            DateTime checkDate = DateTime.Parse(valueString, MyCultureInfo);
+            DateTime curDate = DateTime.Now;
+            /*
+             *            
+             *  curDate     checkDate     parameter   diff   return
+             *              (from list)     (i)               
+             *  ======================================================
+             *  22.05.2019  21.05.2019      1         -1     false
+             *  22.05.2019  22.05.2019      1          0     false
+             *  22.05.2019  23.05.2019      1          1     true
+             * 
+             *  22.05.2019  21.05.2019      0         -1     false
+             *  22.05.2019  22.05.2019      0          0     true
+             *  22.05.2019  23.05.2019      0          1     true
+             * 
+             *  22.05.2019  21.05.2019      1         -1     false
+             *  22.05.2019  22.05.2019      1          0     false
+             *  22.05.2019  23.05.2019      1          1     true
+             * 
+             *  22.05.2019  21.05.2019      5         -1     false
+             *  22.05.2019  22.05.2019      5          0     false
+             *  22.05.2019  23.05.2019      5          1     false
+             *  29.05.2019  23.05.2019      5         -6     true
+             * 
+             *  22.05.2019  21.05.2019      -5        -1     true
+             *  22.05.2019  22.05.2019      -5         0     true
+             *  22.05.2019  23.05.2019      -5         1     true
+             *  29.05.2019  23.05.2019      -5        -6     false
+             * 
+             */
+            // TimeSpan span = curDate.Subtract(checkDate);
+            TimeSpan span = checkDate.Subtract(curDate);
+            int diff = (int)span.Days;
+
+            i = int.Parse(parameter.ToString());
+            if (diff >= i)
+                return true;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
